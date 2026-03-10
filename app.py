8f93b8f6886706cd473196ba3e189990c04f3471
Phusanisa <6714951033@rbru.ac.th>
2026-03-10 23:22:03 +0700

    add web interface

---
+++ app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from PIL import Image
+import io
+from model import predict
+
+app = FastAPI()
+
+# เปิดโฟลเดอร์ static
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# หน้าเว็บหลัก
+@app.get("/")
+def home():
+    return FileResponse("static/index.html")
+
+
+# API สำหรับ AI
+@app.post("/predict")
+async def classify(file: UploadFile = File(...)):
+
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents)).convert("RGB")
+
+    result = predict(image)
+
+    return {"prediction": result}
---
+++ model.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+# โหลดโมเดล
+model = tf.keras.applications.MobileNetV2(weights="imagenet")
+
+def predict(image: Image):
+
+    image = image.resize((224, 224))
+    image = np.array(image)
+
+    image = tf.keras.applications.mobilenet_v2.preprocess_input(image)
+    image = np.expand_dims(image, axis=0)
+
+    predictions = model.predict(image)
+
+    decoded = tf.keras.applications.mobilenet_v2.decode_predictions(predictions, top=1)
+
+    return decoded[0][0][1]
